e3c3d95aaffce4d3e7705a2bc38b4f6213a4bde1
Joshna Suresh <joshnasuresh@Mac.lan>
2025-03-16 14:10:12 -0700

    initial commit

---
+++ contracts/zombiefactory.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.25;
+
+import "./ownable.sol";
+import "./safemath.sol";
+
+contract ZombieFactory is Ownable {
+
+  using SafeMath for uint256;
+  using SafeMath32 for uint32;
+  using SafeMath16 for uint16;
+
+  event NewZombie(uint zombieId, string name, uint dna);
+
+  uint dnaDigits = 16; // Default, can be changed
+  uint dnaModulus = 10 ** dnaDigits; // Default, can be changed
+  uint cooldownTime = 1 days; // Default, can be changed
+
+  struct Zombie {
+    string name;
+    uint dna;
+    uint32 level;
+    uint32 readyTime;
+    uint16 winCount;
+    uint16 lossCount;
+  }
+
+  Zombie[] public zombies;
+
+  mapping (uint => address) public zombieToOwner;
+  mapping (address => uint) ownerZombieCount;
+
+  // New function to allow changing of cooldownTime, dnaDigits, and dnaModulus
+  function setCooldownTime(uint _time) external onlyOwner {
+    cooldownTime = _time;
+  }
+
+  function setDnaDigits(uint _digits) external onlyOwner {
+    dnaDigits = _digits;
+    dnaModulus = 10 ** dnaDigits;  // Update modulus based on new digits
+  }
+
+  function _createZombie(string _name, uint _dna) internal {
+    uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+    zombieToOwner[id] = msg.sender;
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+    emit NewZombie(id, _name, _dna);
+  }
+
+  function _generateRandomDna(string _str) private view returns (uint) {
+    uint rand = uint(keccak256(abi.encodePacked(_str)));
+    return rand % dnaModulus;
+  }
+
+  function createRandomZombie(string _name) public {
+    require(ownerZombieCount[msg.sender] == 0);
+    uint randDna = _generateRandomDna(_name);
+    randDna = randDna - randDna % 100;
+    _createZombie(_name, randDna);
+  }
+}
